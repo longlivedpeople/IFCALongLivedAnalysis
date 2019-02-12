4bb3d9fb5082c95c1328fed3ed611abe0afcf92d
Celia Fernandez Madrazo <celiaafernandez95@gmail.com>
2019-02-12 18:31:47 +0100

    Create output.root and include some muon features there

--- plugins/LongLivedAnalysis.cc
+++ plugins/LongLivedAnalysis.cc
@@ -39,6 +39,26 @@
 
 #include <string>
 
+#include "TLorentzVector.h"
+#include "TTree.h"
+#include "TFile.h"
+
+
+//=======================================================================================================================================================================================================================//
+/////// -----------------------> DATA DEFINITION
+
+////////// BRANCHES
+
+// Muon Selection
+const Int_t nMuonMax = 100;
+Int_t nMuon;
+Float_t MuonSel_pt[nMuonMax];
+Float_t MuonSel_eta[nMuonMax];
+
+
+////////// OUTPUT FILE AND OUTPUT TREE
+TFile *file_out;
+TTree *tree_out;
 
 
 //=======================================================================================================================================================================================================================//
@@ -55,7 +75,7 @@ class LongLivedAnalysis : public edm::one::EDAnalyzer<edm::one::SharedResources>
       virtual void analyze(const edm::Event&, const edm::EventSetup&) override;
       virtual void endJob() override;
 
-      std::string theFileName;
+      std::string output_filename;
       edm::ParameterSet parameters;
       edm::EDGetTokenT<edm::View<pat::Muon> >  theMuonCollection;   
       edm::EDGetTokenT<edm::View<pat::Photon> > thePhotonCollection;
@@ -76,7 +96,6 @@ LongLivedAnalysis::LongLivedAnalysis(const edm::ParameterSet& iConfig)
    theMuonCollection = consumes<edm::View<pat::Muon> >  (parameters.getParameter<edm::InputTag>("MuonCollection"));
    thePhotonCollection = consumes<edm::View<pat::Photon> > (parameters.getParameter<edm::InputTag>("PhotonCollection"));
    theIsoTrackCollection = consumes<edm::View<pat::IsolatedTrack> >  (parameters.getParameter<edm::InputTag>("IsoTrackCollection"));
-   theFileName = parameters.getParameter<std::string>("nameOfFile");
  
 }
 //=======================================================================================================================================================================================================================//
@@ -97,6 +116,7 @@ LongLivedAnalysis::~LongLivedAnalysis()
 void LongLivedAnalysis::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
 {
 
+   /////////// GET THE COLLECTIONS
    edm::Handle<edm::View<pat::Muon> > muons;
    edm::Handle<edm::View<pat::Photon> > photons;
    edm::Handle<edm::View<pat::IsolatedTrack> > isotracks;
@@ -105,15 +125,25 @@ void LongLivedAnalysis::analyze(const edm::Event& iEvent, const edm::EventSetup&
    iEvent.getByToken(thePhotonCollection, photons);
    iEvent.getByToken(theIsoTrackCollection, isotracks);
 
+
+
+   //////////// MUON FEATURES
+
    // Loop over the muons
+   nMuon = muons->size();
    for (size_t i = 0; i < muons->size(); ++i){
 
        const pat::Muon & muon = (*muons)[i];
        float pt = muon.pt();
-
+       MuonSel_pt[i] = pt;
+       MuonSel_eta[i] = muon.eta(); 
        std::cout << pt << std::endl;
    }
 
+
+
+   //////////// FILL THE TREE
+   tree_out->Fill();
 
 }
 //=======================================================================================================================================================================================================================//
@@ -124,6 +154,22 @@ void LongLivedAnalysis::analyze(const edm::Event& iEvent, const edm::EventSetup&
 //=======================================================================================================================================================================================================================//
 void LongLivedAnalysis::beginJob()
 {
+
+    // Output file definition
+    output_filename = parameters.getParameter<std::string>("nameOfOutput");
+    file_out = new TFile(output_filename.c_str(), "RECREATE");
+    file_out->cd();
+    
+    // Output Tree definition
+    tree_out = new TTree("Events", "Events");
+
+
+    // Muon branches definition
+    tree_out->Branch("nMuon", &nMuon, "nMuon/I");
+    tree_out->Branch("MuonSel_pt", MuonSel_pt, "MuonSel_pt[nMuon]/F");
+    tree_out->Branch("MuonSel_eta", MuonSel_eta, "MuonSel_pt[nMuon]/F");
+
+
 }
 //=======================================================================================================================================================================================================================//
 
@@ -133,6 +179,10 @@ void LongLivedAnalysis::beginJob()
 //=======================================================================================================================================================================================================================//
 void LongLivedAnalysis::endJob() 
 {
+
+    tree_out->Write();
+    file_out->Close();
+
 }
 //=======================================================================================================================================================================================================================//
 
--- python/IFCALongLivedAnalysis_cff.py
+++ python/IFCALongLivedAnalysis_cff.py
@@ -2,7 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 
 longlivedanalyzer = cms.EDAnalyzer('LongLivedAnalysis',
-    nameOfFile = cms.string('output.root'),
+    nameOfOutput = cms.string('output.root'),
     MuonCollection = cms.InputTag("slimmedMuons"),
     PhotonCollection = cms.InputTag("slimmedPhotons"),
     IsoTrackCollection = cms.InputTag("isolatedTracks")
