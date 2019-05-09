89518a9d93f6d893221a4996c79686f496fe3096
Celia Fernandez Madrazo <celiaafernandez95@gmail.com>
2019-05-09 09:52:54 +0200

    Correct running file

--- test/runLongLivedAnalysis_cfg.py
+++ test/runLongLivedAnalysis_cfg.py
@@ -18,9 +18,8 @@ process.source = cms.Source("PoolSource",
         #"file:/afs/cern.ch/work/p/pablom/public/1686A035-14E9-E811-BCC8-0242AC130002.root"
         #'file:/afs/cern.ch/work/f/fernance/private/Long_Lived_Analysis/CMSSW_9_4_4/src/Samples/DisplacedSUSY/miniAOD/EXO-DisplacedSUSY_squarkToQuarkChi_MSquark_1500_MChi_494_ctau_160mm_TuneCP5_14TeV_pythia8_miniAOD_N10000.root'
         #'file:test/DisplacedSUSY_squarkToQuarkChi_MSquark_1500_MChi_494_ctau_160mm_TuneCP2_13TeV_pythia8_80X_v1_MiniAODv3_994.root'
-        'dbs:/DisplacedSUSY_squarkToQuarkChi_MSquark_1500_MChi_494_ctau_160mm_TuneCP2_13TeV_pythia8_80X_v1/fernance-DisplacedSUSY_squarkToQuarkChi_MSquark_1500_MChi_494_ctau_160mm_TuneCP2_13TeV_v1_MiniAODv3-bd3e7bcff6c9bcad356ea4ed7e4f08b4/USER'
-        #'file:/afs/cern.ch/work/f/fernance/private/Long_Lived_Analysis/CMSSW_9_4_9/src/2016/MiniAODv3/HIG-RunIISummer15GS-01425_4_MiniAODv3.root'
-        #'file:/afs/cern.ch/work/p/pablom/public/1686A035-14E9-E811-BCC8-0242AC130002.root'
+       # 'file:/afs/cern.ch/work/f/fernance/private/Long_Lived_Analysis/CMSSW_9_4_9/src/2016/MiniAODv3/HIG-RunIISummer15GS-01425_4_MiniAODv3.root'
+        'file:/afs/cern.ch/work/p/pablom/public/1686A035-14E9-E811-BCC8-0242AC130002.root'
     )
 )
 
