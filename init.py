f57e96835bb54f5f48b6d734edd4e14440dbd051
CeliaFernandez <celia.fernandez.madrazo@cern.ch>
2020-04-28 13:09:15 +0200

    update init.py

--- init.py
+++ init.py
@@ -10,7 +10,8 @@ IMPORTANT: This script should be run before running the analyzer
 ####################################
 print('>> Accessing /eos/user/f/fernance/LLP_Analysis/...')
 
-shutil.copy('/eos/user/f/fernance/LLP_Analysis/PUreweighting/2016DataPileupHistogram.root', 'test/2016/PUreweighting')
+shutil.copy('/eos/user/f/fernance/LLP_Analysis/PUreweighting/2016DataPileupHistogram.root', 'test/2016/PUreweighting/')
+shutil.copy('/eos/user/f/fernance/LLP_Analysis/PUreweighting/2016MCPileupHistogram.root', 'test/2016/PUreweighting/')
 shutil.copy('/eos/user/f/fernance/LLP_Analysis/PUreweighting/2016DataPileupHistogram.root', './')
 shutil.copy('/eos/user/f/fernance/LLP_Analysis/PUreweighting/2016MCPileupHistogram.root', './')
 
