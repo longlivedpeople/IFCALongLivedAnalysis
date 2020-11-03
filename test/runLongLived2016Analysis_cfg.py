a43a2cd2a09af855ad76618200357d920099242e
CeliaFernandez <celia.fernandez.madrazo@cern.ch>
2020-11-03 10:40:22 +0100

    Split running files into eras

---
+++ test/runLongLived2016Analysis_cfg.py
@@ -0,0 +1,31 @@
+import FWCore.ParameterSet.Config as cms
+
+
+process = cms.Process("Demo")
+
+process.load("FWCore.MessageService.MessageLogger_cfi")
+process.load("MyAnalysis.IFCALongLivedAnalysis.IFCALongLivedAnalysis_cff")
+process.load("TrackingTools/TransientTrack/TransientTrackBuilder_cfi")
+#process.load("Configuration.Geometry.GeometryIdeal_cff")
+process.load('Configuration.Geometry.GeometryRecoDB_cff')
+process.load("Configuration.StandardSequences.MagneticField_cff")
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+process.GlobalTag.globaltag = '94X_mcRun2_asymptotic_v3'  # or some other global tag depending on your CMSSW release and sample. 
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(5000) )
+
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring(
+        #"file:/afs/cern.ch/work/p/pablom/public/1686A035-14E9-E811-BCC8-0242AC130002.root"
+       #'file:/afs/cern.ch/work/p/pablom/public/1686A035-14E9-E811-BCC8-0242AC130002.root'
+       # 'file:test/merged.root'
+       [
+          #'file:/eos/user/f/fernance/LLPNTuples/2016/test/HXX_400_50_400_test.root',
+          'file:/eos/user/f/fernance/LLP_Analysis/miniAOD_extended/DY_test/EXO-RunIISummer16MiniAODv3-08121_324.root',
+          'file:/eos/user/f/fernance/LLP_Analysis/miniAOD_extended/DY_test/EXO-RunIISummer16MiniAODv3-08121_325.root',
+       ]
+    )
+)
+
+process.p = cms.Path(process.longlivedanalyzer)
+
+
