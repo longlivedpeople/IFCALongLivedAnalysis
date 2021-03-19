544df393660f99785a3ae26811f0cfba7344a2cd
CeliaFernandez <celia.fernandez.madrazo@cern.ch>
2021-03-19 17:36:38 +0100

    Update the 2016 crab configuration to run on top of UL datasets

--- test/2016/runLongLived2016postVFP_DATA_EG_cfg.py
+++ test/2016/runLongLived2016postVFP_DATA_EG_cfg.py
@@ -6,18 +6,14 @@ process = cms.Process("Demo")
 process.load("FWCore.MessageService.MessageLogger_cfi")
 process.load("MyAnalysis.IFCALongLivedAnalysis.IFCALongLivedAnalysis_cff")
 process.load("TrackingTools/TransientTrack/TransientTrackBuilder_cfi")
-#process.load("Configuration.Geometry.GeometryIdeal_cff")
 process.load('Configuration.Geometry.GeometryRecoDB_cff')
 process.load("Configuration.StandardSequences.MagneticField_cff")
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
-process.GlobalTag.globaltag = '94X_dataRun2_v10'  # or some other global tag depending on your CMSSW release and sample. 
+process.GlobalTag.globaltag = '106X_dataRun2_v32'  # or some other global tag depending on your CMSSW release and sample. 
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(50) )
 
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
-        #"file:/afs/cern.ch/work/p/pablom/public/1686A035-14E9-E811-BCC8-0242AC130002.root"
-       #'file:/afs/cern.ch/work/p/pablom/public/1686A035-14E9-E811-BCC8-0242AC130002.root'
-       # 'file:test/merged.root'
        [
 'file:/eos/user/f/fernance/LLP_Analysis/miniAOD_extended/DoubleMuon_test/EXO-RunIISummer16MiniAODv3-08121_325.root'
        ]
--- test/2016/runLongLived2016postVFP_MCAnalysis_cfg.py
+++ test/2016/runLongLived2016postVFP_MCAnalysis_cfg.py
@@ -6,27 +6,23 @@ process = cms.Process("Demo")
 process.load("FWCore.MessageService.MessageLogger_cfi")
 process.load("MyAnalysis.IFCALongLivedAnalysis.IFCALongLivedAnalysis_cff")
 process.load("TrackingTools/TransientTrack/TransientTrackBuilder_cfi")
-#process.load("Configuration.Geometry.GeometryIdeal_cff")
 process.load('Configuration.Geometry.GeometryRecoDB_cff')
 process.load("Configuration.StandardSequences.MagneticField_cff")
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
-process.GlobalTag.globaltag = '94X_dataRun2_v10'  # or some other global tag depending on your CMSSW release and sample. 
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(50) )
+process.GlobalTag.globaltag = '106X_mcRun2_asymptotic_v15'  # or some other global tag depending on your CMSSW release and sample. 
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(5000) )
 
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
-        #"file:/afs/cern.ch/work/p/pablom/public/1686A035-14E9-E811-BCC8-0242AC130002.root"
-       #'file:/afs/cern.ch/work/p/pablom/public/1686A035-14E9-E811-BCC8-0242AC130002.root'
-       # 'file:test/merged.root'
        [
-'file:/eos/user/f/fernance/LLP_Analysis/miniAOD_extended/DoubleMuon_test/problematic/EXO-RunIISummer16MiniAODv3-08121_854.root'
+          'das://H2ToLLPXToLeptons_MH_400_MX_50_ctau_400mm_TuneCP2_13TeV_pythia8_80X_13082019-1313/fernance-400-50-400_HXX_RunIISummer16MiniAODv3_230220-1650-bd3e7bcff6c9bcad356ea4ed7e4f08b4/USER'
        ]
-    ),
-    skipEvents = cms.untracked.uint32(0)
+    )
 )
 
-process.longlivedanalyzer.isData = True
+process.longlivedanalyzer.isData = False
 process.longlivedanalyzer.DSAMode = True
+process.longlivedanalyzer.BSMode = False
 process.longlivedanalyzer.Era = 2016
 
 process.p = cms.Path(process.longlivedanalyzer)
